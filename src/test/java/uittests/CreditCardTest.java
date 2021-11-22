7c5c061a97bd720bbc8b1e778b6027748edbb426
Konul Salimova <salimovakonul@gmail.com>
2021-11-22 01:54:37 -0500

    raw code

---
+++ src/test/java/uittests/CreditCardTest.java
@@ -0,0 +1,18 @@
+package uittests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import pages.CreditCardPage;
+
+public class CreditCardTest extends TestBase {
+    WebDriver driver;
+
+    public void navigateCreditCardOverviewPage() {
+        CreditCardPage creditCard = new CreditCardPage();
+        creditCard.enterData();
+
+
+
+    }
+
+}
